84cf07b58fd40e29bd61541d6755a9decacf4fad
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-05 15:37:18 -0300

    feat: Resposta do Exercicio1

--- src/implementacao/1-soma-maior-que-50.ts
+++ src/implementacao/1-soma-maior-que-50.ts
@@ -1,3 +1,13 @@
-function somaMaiorQueCinquenta() {
-    return null;
+function somaMaiorQueCinquenta(valor1: number, valor2: number): number{
+    
+    var soma: number = 0
+
+    soma = valor1 + valor2; 
+    if ( soma > 50 ){
+        return soma
+    }  else {
+        return 0;
+    }
+    
 }
+console.log(somaMaiorQueCinquenta(46,5));
