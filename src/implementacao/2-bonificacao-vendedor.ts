b5d173301e0b92a32bca526859434565077772b8
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-05 15:50:06 -0300

    feat: Resposta do Exercicio2

--- src/implementacao/2-bonificacao-vendedor.ts
+++ src/implementacao/2-bonificacao-vendedor.ts
@@ -1,4 +1,15 @@
-function calcularBonificacao() {
-    return null;
+function calcularBonificacao(valorUni: number, qtdVendida: number): string {
+    
+    let total: number = 0;
+
+    total = valorUni * qtdVendida;
+
+    if (total > 100){
+        return "Bonificação de 10% para o vendedor!"
+    } else {
+        return "Você não tem bonificação."
+    }
+    
 }
+console.log(calcularBonificacao(10,10));
 
