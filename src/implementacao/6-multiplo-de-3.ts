62a0d8836a5202568508c2c6b9197886a840dd4f
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-06 11:55:57 -0300

    feat: Resposta do Exercicio6

--- src/implementacao/6-multiplo-de-3.ts
+++ src/implementacao/6-multiplo-de-3.ts
@@ -1,4 +1,19 @@
 function verificarMultiploDe3() {
-    return null;
-}
+    const readline = require('readline');
+    const teclado = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
+    teclado.question("Digite um numero: ", (numero: string) => {
+        const numeroConvertido = parseInt(numero);
+
+        if ( numeroConvertido % 3 == 0){
+            console.log("É múltiplo de 3")
+        } else {
+            console.log("Não é múltiplo de 3")
+        }
+        teclado.close();
+    })
+}
+verificarMultiploDe3();
