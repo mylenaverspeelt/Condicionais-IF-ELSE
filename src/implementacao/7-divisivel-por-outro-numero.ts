fa825e99cd28473551892668dce65ebef37f3c1e
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-06 13:17:07 -0300

    feat: Resposta do Exercicio7

--- src/implementacao/7-divisivel-por-outro-numero.ts
+++ src/implementacao/7-divisivel-por-outro-numero.ts
@@ -1,4 +1,24 @@
 function verificarDivisivel() {
-    return null;
-}
+  const readline = require("readline");
 
+  const teclado = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  teclado.question("Digite o primeiro numero: ", (primeiroNumero: string) => {
+    teclado.question("Digite o segundo numero: ", (segundoNumero: string) => {
+      let numero1 = parseInt(primeiroNumero);
+      let numero2 = parseInt(segundoNumero);
+
+      if (numero1 % numero2 == 0) {
+        console.log("Esse numero é divisível");
+      } else {
+        console.log("Esse numero não é divisível");
+      }
+
+      teclado.close();
+    });
+  });
+}
+verificarDivisivel();
