17b1520e59855b8ca5d76c0dc07f9d8bc8378d60
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-06 11:03:44 -0300

    feat: Resposta do Exercicio4

--- src/implementacao/4-menor-maior-valor.ts
+++ src/implementacao/4-menor-maior-valor.ts
@@ -1,4 +1,25 @@
 function determinarMenorMaior() {
-    return null;
-}
+  const readline = require("readline");
 
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question(" Digite o primeiro numero: ", (primeiroNumero: string) => {
+    let numero1 = parseInt(primeiroNumero);
+
+    rl.question(" Digite o segundo numero: ", (segundoNumero: string) => {
+      let numero2 = parseInt(segundoNumero);
+
+      if (numero1 > numero2) {
+        console.log(`O numero ${numero1} é maior do que o numero ${numero2} `);
+      } else {
+        console.log(`O numero ${numero2} é maior do que o numero ${numero1} `);
+      }
+      rl.close();
+    });
+  });
+    
+}
+determinarMenorMaior();
