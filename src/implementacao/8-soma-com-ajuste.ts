c423e25022cb62fb51d72d43d12ac868c95039f5
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-06 13:47:26 -0300

    feat: Resposta do Exercicio8

--- src/implementacao/8-soma-com-ajuste.ts
+++ src/implementacao/8-soma-com-ajuste.ts
@@ -1,4 +1,25 @@
 function somaComAjuste() {
-    return null;
-}
+    const readline = require('readline');
 
+    const teclado = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    teclado.question("Digite o primeiro numero: ", (primeiroNumero: string) => {
+      teclado.question("Digite o segundo numero: ",(segundoNumero: string) => {
+        let numero1 = parseInt(primeiroNumero);
+        let numero2 = parseInt(segundoNumero);
+
+        let soma = numero1 + numero2
+
+        if (soma > 20){
+            console.log(soma + 8)
+        } else {
+            console.log(soma - 5)
+        }
+        teclado.close();
+      })  
+    })
+}
+somaComAjuste();
