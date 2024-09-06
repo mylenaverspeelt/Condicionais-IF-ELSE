e4e2b812c5c5b99a4e17a380a9ff02c0a52e8ccf
mylenaverspeelt <mylena49@hotmail.com>
2024-09-06 10:23:51 -0300

    feat: Resposta do Exercicio3

--- src/implementacao/3-numero-positivo-negativo-nulo.ts
+++ src/implementacao/3-numero-positivo-negativo-nulo.ts
@@ -1,4 +1,25 @@
 function verificarNumero() {
-    return null;
+    const readline = require('readline');
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('Digite um numero: ', (numeroInput: string) => {
+        const numeroConvertido = parseInt(numeroInput)
+
+        if (numeroConvertido < 0) {
+            console.log("Negativo")
+        } else if (numeroConvertido == 0) {
+            console.log("Nulo")
+        } else {
+            console.log("Positivo")
+        }
+        rl.close()
+    });
 }
+
+verificarNumero()
+
 
