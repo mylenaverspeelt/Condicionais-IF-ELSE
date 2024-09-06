e49ba929357639bd8ef91ef62a4f25869f81fc0e
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-06 11:35:44 -0300

    feat: Resposta do Exercicio5

--- src/implementacao/5-par-ou-impar.ts
+++ src/implementacao/5-par-ou-impar.ts
@@ -1,4 +1,20 @@
 function verificarParOuImpar() {
-    return null;
-}
+  const readline = require("readline");
+  const teclado = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
+  teclado.question("Digite um numero: ", (numero: string ) => {
+    
+    let numeroConvertido = parseInt(numero);
+    
+    if (numeroConvertido % 2 == 0){
+        console.log("O numero digitado é PAR");
+    } else {
+        console.log("O numero digitado é IMPAR");
+    }
+    teclado.close();
+  });
+}
+verificarParOuImpar()
