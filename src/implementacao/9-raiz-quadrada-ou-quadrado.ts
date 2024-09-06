9f3ae1178f1dd43e0c4b58cf34c70aa8aac3c114
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-06 14:21:55 -0300

    feat: Resposta do Exercicio9

--- src/implementacao/9-raiz-quadrada-ou-quadrado.ts
+++ src/implementacao/9-raiz-quadrada-ou-quadrado.ts
@@ -1,4 +1,20 @@
 function calcularRaizOuQuadrado() {
-    return null;
-}
+  const readline = require("readline");
 
+  const teclado = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  teclado.question("Digite um numero: ", (valorDigitado: string) => {
+    let numero = parseInt(valorDigitado);
+
+    if (numero >= 0) {
+        console.log("A raiz quadrada do numero é: ", Math.sqrt(numero))
+    } else {
+        console.log("O quadrado do numero é: ", (numero*numero))
+    }
+    teclado.close();
+  });
+}
+calcularRaizOuQuadrado();
