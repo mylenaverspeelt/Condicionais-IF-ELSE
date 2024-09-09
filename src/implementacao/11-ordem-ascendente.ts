3514e4f91f5d2b973241f08cd57311d4b1f6d242
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-09 11:21:42 -0300

    feat: Resposta do Exercicio11 parte 1

--- src/implementacao/11-ordem-ascendente.ts
+++ src/implementacao/11-ordem-ascendente.ts
@@ -1,4 +1,45 @@
 function ordenarAscendente() {
-    return null;
-}
+  const readline = require("readline");
+  const teclado = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
+  teclado.question("Digite o primeiro numero: ", (primeiroNumero: string) => {
+    let numero1 = parseInt(primeiroNumero);
+    teclado.question("Digite o segundo numero: ", (segundoNumero: string) => {
+      let numero2 = parseInt(segundoNumero);
+      teclado.question(
+        "Digite o terceiro numero: ",
+        (terceiroNumero: string) => {
+          let numero3 = parseInt(terceiroNumero);
+
+          let arrayNumeros: number[] = [numero1, numero2, numero3];
+
+          let numeroMaior: number = 0;
+          let numeroMenor: number = 0;
+          let numeroMeio: number = 0;
+
+          numeroMenor = arrayNumeros[0];
+          
+
+          for (let i = 0; i < arrayNumeros.length; i++) {
+            if (numeroMaior < arrayNumeros[i]) {
+              numeroMaior = arrayNumeros[i];
+
+            } else if ( arrayNumeros[i] < numeroMenor) {
+              numeroMenor = arrayNumeros[i];
+
+            } else {
+                numeroMeio = arrayNumeros[i];
+            }
+
+            teclado.close();
+          }
+          console.log(numeroMenor,numeroMeio, numeroMaior);
+        }
+      );
+    });
+  });
+}
+ordenarAscendente();
