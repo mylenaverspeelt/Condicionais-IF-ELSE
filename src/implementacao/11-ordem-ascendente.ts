d8d87ef43ffd94975c8a044da802eff5d658f0b9
mylenaverspeelt <mylena49@hotmail.com>
2024-09-09 14:48:39 -0300

    fix: Resposta do Exercicio11 final

--- src/implementacao/11-ordem-ascendente.ts
+++ src/implementacao/11-ordem-ascendente.ts
@@ -13,18 +13,24 @@ function ordenarAscendente() {
         "Digite o terceiro numero: ",
         (terceiroNumero: string) => {
           let numero3 = parseInt(terceiroNumero);
- 
+
           if (numero1 <= numero2 && numero1 <= numero3) {
             if (numero2 <= numero3) {
               console.log(numero1, numero2, numero3)
             } else {
               console.log(numero1, numero3, numero2)
             }
-          } else {
-            if (numero2 >= numero3) {
-              console.log(numero3, numero2, numero1)
+          } else if (numero2 <= numero1 && numero2 <= numero3) {
+            if (numero1 <= numero3) {
+              console.log(numero2, numero1, numero3)
             } else {
               console.log(numero2, numero3, numero1)
+            }
+          } else {
+            if (numero1 <= numero2) {
+              console.log(numero3, numero1, numero2)
+            } else {
+              console.log(numero3, numero2, numero1)
             }
           }
 
