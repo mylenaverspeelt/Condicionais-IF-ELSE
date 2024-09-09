5b13492136ccbfc6a993ec868e4293052d958f01
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-09 16:00:02 -0300

    feat: Resposta do Exercicio13 parte 1

--- src/implementacao/13-emprestimo-salario.ts
+++ src/implementacao/13-emprestimo-salario.ts
@@ -1,3 +1,17 @@
 function verificarEmprestimo() {
-    return null;
-}
+    const readline = require("readline");
+    const teclado = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    teclado.question("Digite seu salário bruto: ", (valorSalario: string) => {
+        let salarioBruto = parseFloat(valorSalario);
+        teclado.question("Digite o valor da prestação: ", (valorPrestacao: string) => {
+            let prestacao = parseFloat(valorPrestacao);
+
+            
+        })
+    }) 
+        }
+
