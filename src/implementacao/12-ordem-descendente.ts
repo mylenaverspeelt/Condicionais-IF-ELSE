121d36e23440986baedb8247e08bd376f3ed1c74
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-09 15:15:12 -0300

    feat: Resposta do Exercicio12

--- src/implementacao/12-ordem-descendente.ts
+++ src/implementacao/12-ordem-descendente.ts
@@ -1,3 +1,43 @@
 function ordenarDescendente() {
-    return null;
-}
+    const readline = require("readline");
+    const teclado = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+  
+    teclado.question("Digite o primeiro numero: ", (primeiroNumero: string) => {
+      let numero1 = parseInt(primeiroNumero);
+      teclado.question("Digite o segundo numero: ", (segundoNumero: string) => {
+        let numero2 = parseInt(segundoNumero);
+        teclado.question(
+          "Digite o terceiro numero: ",
+          (terceiroNumero: string) => {
+            let numero3 = parseInt(terceiroNumero);
+  
+            if (numero1 >= numero2 && numero1 >= numero3) {
+              if (numero2 >= numero3) {
+                console.log(numero1, numero2, numero3)
+              } else {
+                console.log(numero1, numero3, numero2)
+              }
+            } else if (numero2 >= numero1 && numero2 >= numero3) {
+              if (numero1 >= numero3) {
+                console.log(numero2, numero1, numero3)
+              } else {
+                console.log(numero2, numero3, numero1)
+              }
+            } else {
+              if (numero1 >= numero2) {
+                console.log(numero3, numero1, numero2)
+              } else {
+                console.log(numero3, numero2, numero1)
+              }
+            }
+  
+            teclado.close();
+          }
+        );
+      });
+    });
+  }
+  ordenarDescendente();
