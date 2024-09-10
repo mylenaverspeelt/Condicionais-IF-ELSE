f5a588ed55694d97776f666e0804fec9047e5ede
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-10 09:38:46 -0300

    feat: Resposta do Exercicio13 Final

--- src/implementacao/13-emprestimo-salario.ts
+++ src/implementacao/13-emprestimo-salario.ts
@@ -1,17 +1,27 @@
 function verificarEmprestimo() {
-    const readline = require("readline");
-    const teclado = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
+  const readline = require("readline");
+  const teclado = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-    teclado.question("Digite seu salário bruto: ", (valorSalario: string) => {
-        let salarioBruto = parseFloat(valorSalario);
-        teclado.question("Digite o valor da prestação: ", (valorPrestacao: string) => {
-            let prestacao = parseFloat(valorPrestacao);
+  teclado.question("Digite seu salário bruto: ", (valorSalario: string) => {
+    let salarioBruto = parseFloat(valorSalario);
+    teclado.question(
+      "Digite o valor da prestação: ",
+      (valorPrestacao: string) => {
+        let prestacao = parseFloat(valorPrestacao);
 
-            
-        })
-    }) 
+        const valorMaximo: number = salarioBruto * 0.3;
+
+        if (prestacao > valorMaximo) {
+          console.log("O empréstimo não pode ser concedido.");
+        } else {
+          console.log("Empréstimo concedido.");
         }
-
+        teclado.close();
+      }
+    );
+  });
+}
+verificarEmprestimo();
